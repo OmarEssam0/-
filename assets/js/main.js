4261f782bb89db6bdac3fe843b9186981fd2f3b4
OmarEssam0 <Omar_Essam0@outlook.com>
2024-09-01 01:20:06 +0300

    aa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,35 @@
-let navBtn = document.getElementById("nav-btn")
-let icon = document.getElementById("icon")
-let close = document.getElementsByClassName("fa-xmark")
-let showNav = document.getElementById("navClick")
-let prod = document.getElementById("prod")
-let Brands = document.querySelectorAll(".Brands")
-
-
-function changeIconBtn(){
-    if(icon.className.includes("fa-bars-staggered"))icon.classList.replace("fa-bars-staggered" , "fa-xmark");
-        else icon.classList.replace("fa-xmark" , "fa-bars-staggered");
-
-}
-function showLinks(){
-    if(icon.className.includes("fa-xmark")){
-        showNav.classList.replace("d-none" , "d-block")
-    }else{
-        showNav.classList.replace("d-block" , "d-none")
+    let navBtn = document.getElementById("nav-btn"),
+        icon = document.getElementById("icon"),
+        close = document.getElementsByClassName("fa-xmark"),
+        showNav = document.getElementById("navClick"),
+        prod = document.getElementById("prod"),
+        Brands = document.querySelectorAll(".Brands"),
+        navbarMobile = document.querySelector(".navbar-mobile");
+    function changeIconBtn() {
+        icon.className.includes("fa-bars-staggered") ? icon.classList.replace("fa-bars-staggered", "fa-xmark") : icon.classList.replace("fa-xmark", "fa-bars-staggered");
     }
-}
-navBtn.addEventListener("click" , function(){
-    changeIconBtn()
-    showLinks()
-})
+    function showLinks() {
+        icon.className.includes("fa-xmark") ? showNav.classList.replace("d-none", "d-block") : showNav.classList.replace("d-block", "d-none");
+    }
+    navBtn.addEventListener("click", function () {
+        changeIconBtn(), showLinks();
+    }),
+        prod.addEventListener("mouseover", function () {
+            document.getElementById("product").classList.remove("d-none");
+        });
+    for (let i = 0; i < Brands.length; i++)
+        Brands[i].addEventListener("click", function () {
+            document.getElementById("cars").classList.toggle("d-none");
+        });
 
-prod.addEventListener("click" ,function(){
-        document.getElementById("product").classList.toggle("d-none")  
-})
+        document.querySelector(".nav-bt-mobile").addEventListener("click" , function(){
+            navbarMobile.classList.replace("d-none" , "d-block")
+        })
+        document.querySelector(".fa-xmark").addEventListener("click" , function(){
+            navbarMobile.classList.replace("d-block" , "d-none")
 
-for(let i = 0; i < Brands.length ; i++){
- Brands[i].addEventListener("click" , function(){
-        document.getElementById("cars").classList.toggle("d-none")
-    })
-}
-    
-   
-
-
-
-$(document).ready(function(){
-    $('.your-slider').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      dots: true,
-  fade: true,
-  cssEase: 'linear',
-  rtl: true,
-  arrows: false  // أضف الأسهم إذا كنت بحاجة إليها
-
-    });
-  });
+        })
+$(document).ready(function () {
+    $(".your-slider").slick({ infinite: !0, slidesToShow: 1, slidesToScroll: 1, autoplay: !0, autoplaySpeed: 2e3, dots: !0, fade: !0, cssEase: "linear", rtl: !0, arrows: !1 });
+});
+new WOW().init();
